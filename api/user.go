1da7b32e46416b366b81b1d4eb5f700f4826a95b
Zeljko Lazic <z.lazic94@icloud.com>
2022-05-13 22:16:01 +0200

    password hasing

---
+++ api/user.go
@@ -0,0 +1,92 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
+	"net/http"
+	db "simplebank/db/sqlc"
+	"simplebank/util"
+	"time"
+)
+
+type createUerRequest struct {
+	Username string `json:"username" binding:"required,alphanum"`
+	Password string `json:"password" binding:"required,min=6"`
+	FullName string `json:"full_name" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+}
+
+type createUerResponse struct {
+	Username          string    `json:"username"`
+	FullName          string    `json:"full_name"`
+	Email             string    `json:"email"`
+	PasswordChangedAt time.Time `json:"password_changed_at"`
+	CreatedAt         time.Time `json:"created_at"`
+}
+
+func (server Server) createUser(ctx *gin.Context) {
+	var req createUerRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	hashedPassword, err := util.HashPassword(req.Password)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+	}
+
+	arg := db.CreateUserParams{
+		Username:       req.Username,
+		HashedPassword: hashedPassword,
+		FullName:       req.FullName,
+		Email:          req.Email,
+	}
+
+	user, err := server.store.CreateUser(ctx, arg)
+	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "unique_violation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	rsp := createUerResponse{
+		Username:          user.Username,
+		FullName:          user.FullName,
+		Email:             user.Email,
+		PasswordChangedAt: user.PasswordChangedAt,
+		CreatedAt:         user.CreatedAt,
+	}
+
+	ctx.JSON(http.StatusCreated, rsp)
+}
+
+//type getUserRequest struct {
+//	ID int64 `uri:"id" binding:"required,min=1"`
+//}
+//
+//func (server Server) getUser(ctx *gin.Context) {
+//	var req getUserRequest
+//	if err := ctx.ShouldBindUri(&req); err != nil {
+//		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+//		return
+//	}
+//
+//	user, err := server.store.GetUser(ctx, req.ID)
+//	if err != nil {
+//		if err == sql.ErrNoRows {
+//			ctx.JSON(http.StatusNotFound, errorResponse(err))
+//			return
+//		}
+//		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+//		return
+//	}
+//
+//	ctx.JSON(http.StatusOK, user)
+//}
